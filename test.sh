c7eaaf510c9361d81b19909171b4ff6d52f5511b
Masatake YAMATO <yamato@redhat.com>
2013-07-11 11:07:33 +0900

    Fix a typo in passwords specification

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ for p in pcap/*.pcap; do
     if ! [[ $p == "dont-*" ]] ;then
 	echo -n checking ${p/pcap\//}...
 	pdml=$(basename $p .pcap).pdml
-	tshark -o corosync_totemnet.private_keys:"beta;alpha:example.com" -t e -r $p -T pdml > pdml.$$/$pdml 2> /dev/null
+	tshark -o corosync_totemnet.private_keys:"beta;alpha;example.com" -t e -r $p -T pdml > pdml.$$/$pdml 2> /dev/null
 	if diff -uN <(grep -v pdml pdml/$pdml) <(grep -v pdml pdml.$$/$pdml) > /dev/null; then
 	    echo successful
 	else
